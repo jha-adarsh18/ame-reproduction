0c2889c5722ba89adf8392601ef62f9620b347cc
Adarsh Jha <jhaadarsh350@gmail.com>
2026-09-06 19:37:06 +0530

    imports mdp for locomotion

--- source/ame_reproduction/ame_reproduction/tasks/manager_based/ame_reproduction/mdp/__init__.pyi
+++ source/ame_reproduction/ame_reproduction/tasks/manager_based/ame_reproduction/mdp/__init__.pyi
@@ -8,6 +8,6 @@ __all__ = [
 ]
 
 # Forward stable MDP terms lazily, then override with environment-specific terms below.
-from isaaclab.envs.mdp import *  # noqa: F401, F403
+from isaaclab_tasks.manager_based.locomotion.velocity.mdp import *  # noqa: F401, F403
 
 from .rewards import joint_pos_target_l2
